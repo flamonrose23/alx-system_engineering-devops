5ea09cbf0026ce62211bbf55d4009aaa8d1af1c8
flamonrose23 <fatinemar20@icloud.com>
2024-06-04 16:28:37 -0700

    test task 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-# Fixes a wordpress site running on apache2
+# Fixing php file extension wordpress site running on apache2
 exec {'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; sudo service apache2 restart',
   path    => ['/bin', '/usr/bin', '/usr/sbin']
